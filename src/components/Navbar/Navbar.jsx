b1ee8f928e92348421b7f90131e2981eb0ec7dfa
SHARUN266 <sharunattari266@gmail.com>
2023-01-13 23:36:05 +0530

    Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.css";
+import { BsCart3 } from "react-icons/bs";
+export default function Navbar() {
+  return (
+    <div className="nav">
+      <div>
+        <h2>TeeRex Store</h2>
+      </div>
+      
+    </div>
+  );
+}
