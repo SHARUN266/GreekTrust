0cb2f574a9dcc41698c7a5d38cee3b124711caa2
SHARUN266 <sharunattari266@gmail.com>
2023-01-14 15:51:42 +0530

    filterbtnAdd

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./Search.css";
-import { BiSearch } from "react-icons/bi";
+import { BiSearch ,BiFilterAlt} from "react-icons/bi";
 export default function Search() {
   return (
     <div className="search">
       <input type="text" placeholder="Search for products..." />
       <button>
         <BiSearch color="white" fontSize={"30px"} />
+      </button>
+      <button id="filterBtn">
+        <BiFilterAlt color="white" fontSize={"30px"} />
       </button>
     </div>
   );
