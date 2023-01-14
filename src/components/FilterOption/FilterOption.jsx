f8be54fdb3f867ce4d3628e6fd4766d6912ae20e
SHARUN266 <sharunattari266@gmail.com>
2023-01-14 20:44:54 +0530

    FilterBtnAdd

--- src/components/FilterOption/FilterOption.jsx
+++ src/components/FilterOption/FilterOption.jsx
@@ -11,21 +11,21 @@ export default function FilterOption() {
           name="checkbox-group"
           value="red"
         />
-        <label for="checkbox1">Red</label>
+        <label htmlFor="checkbox1">Red</label>
         <input
           type="checkbox"
           id="checkbox2"
           name="checkbox-group"
           value="blue"
         />
-        <label for="checkbox2">Blue</label>
+        <label htmlFor="checkbox2">Blue</label>
         <input
           type="checkbox"
           id="checkbox3"
           name="checkbox-group"
           value="green"
         />
-        <label for="checkbox3">Green</label>
+        <label htmlFor="checkbox3">Green</label>
       </div>
       <p>Gender</p>
       <div>
@@ -35,14 +35,14 @@ export default function FilterOption() {
           name="checkbox-group"
           value="men"
         />
-        <label for="checkbox1">Men</label>
+        <label htmlFor="checkbox1">Men</label>
         <input
           type="checkbox"
           id="checkbox2"
           name="checkbox-group"
           value="women"
         />
-        <label for="checkbox2">Women</label>
+        <label htmlFor="checkbox2">Women</label>
       </div>
       <p>Price</p>
       <div>
@@ -52,21 +52,21 @@ export default function FilterOption() {
           name="checkbox-group"
           value="0-250"
         />
-        <label for="checkbox1">0-Rs250</label>
+        <label htmlFor="checkbox1">0-Rs250</label>
         <input
           type="checkbox"
           id="checkbox2"
           name="checkbox-group"
           value="251-450"
         />
-        <label for="checkbox2">Rs251-450</label>
+        <label htmlFor="checkbox2">Rs251-450</label>
         <input
           type="checkbox"
           id="checkbox3"
           name="checkbox-group"
           value="450"
         />
-        <label for="checkbox3">Rs450</label>
+        <label htmlFor="checkbox3">Rs450</label>
       </div>
       <p>Type</p>
       <div>
@@ -76,21 +76,21 @@ export default function FilterOption() {
           name="checkbox-group"
           value="polo"
         />
-        <label for="checkbox1">Polo</label>
+        <label htmlFor="checkbox1">Polo</label>
         <input
           type="checkbox"
           id="checkbox2"
           name="checkbox-group"
           value="hoodies"
         />
-        <label for="checkbox2">Hoodie</label>
+        <label htmlFor="checkbox2">Hoodie</label>
         <input
           type="checkbox"
           id="checkbox3"
           name="checkbox-group"
           value="basic"
         />
-        <label for="checkbox3">Basic</label>
+        <label htmlFor="checkbox3">Basic</label>
       </div>
     </div>
   );
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
-import { BiSearch ,BiFilterAlt} from "react-icons/bi";
+import { BiSearch, BiFilterAlt } from "react-icons/bi";
 export default function Search() {
+   function handleModel(){
+    console.log("Hello")
+    document.getElementsByClassName("filter").style=""
+   }
+ 
   return (
     <div className="search">
       <input type="text" placeholder="Search for products..." />
       <button>
         <BiSearch color="white" fontSize={"30px"} />
       </button>
-      <button id="filterBtn">
+      <button onClick={handleModel} id="filterBtn">
         <BiFilterAlt color="white" fontSize={"30px"} />
       </button>
     </div>
